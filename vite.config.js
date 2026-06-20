0ee6f2d5b06184c5a4072edd76c97da652f1007e
pmmanojkumar <pmmanoj2007@gmail.com>
2026-06-20 23:50:46 +0530

    Fix clean URL routing, typography, and auth UI glitches

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,28 @@ export default defineConfig({
     open: true, // Automatically open browser when server starts
     host: true  // Expose to local network
   },
+  plugins: [
+    {
+      name: 'html-clean-urls',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          const url = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
+          const pathname = url.pathname;
+          
+          if (!pathname.includes('.') && pathname !== '/') {
+            if (pathname === '/login') {
+              req.url = '/login.html' + url.search;
+            } else if (pathname === '/signup') {
+              req.url = '/signup.html' + url.search;
+            } else if (pathname === '/forgot-password') {
+              req.url = '/forgot-password.html' + url.search;
+            }
+          }
+          next();
+        });
+      }
+    }
+  ],
   build: {
     rollupOptions: {
       input: {
